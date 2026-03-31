b4b99476a8e99333c2532e6edd673bcc8447030d
synasapmob <sonn51280@gmail.com>
2026-03-31 11:03:46 +0700

    migrate query config limit from smart contract

--- frontend/app/utils/utils.sui.ts
+++ frontend/app/utils/utils.sui.ts
@@ -1,5 +1,7 @@
+import type { BcsType } from "@mysten/sui/bcs";
 import { SuiGrpcClient } from "@mysten/sui/grpc";
 import { getJsonRpcFullnodeUrl } from "@mysten/sui/jsonRpc";
+import { Transaction } from "@mysten/sui/transactions";
 
 const networkConfig = {
   mainnet: {
@@ -23,8 +25,30 @@ const programs = {
   package: import.meta.env.VITE_ARCHIVE_PACKAGE_ID,
 };
 
+const commandResults = async (target: string, type: BcsType<any>) => {
+  const tx = new Transaction();
+
+  tx.moveCall({
+    target: `${programs.package}::${target}`,
+  });
+
+  const result = await getSuiClient.simulateTransaction({
+    transaction: tx,
+    include: {
+      commandResults: true,
+    },
+  });
+
+  if (result.$kind === "FailedTransaction") {
+    throw "request file config is failed";
+  }
+
+  return type.parse(result.commandResults[0].returnValues[0].bcs);
+};
+
 export default {
   networkConfig,
   getSuiClient,
   programs,
+  commandResults,
 };
