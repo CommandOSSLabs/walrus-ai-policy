44e73f7e7f3187c9d876168638d817fd0b90bbd9
synasapmob <sonn51280@gmail.com>
2026-04-06 11:17:35 +0700

    markdown should be break all

--- frontend/app/layout/Artifact/ArtifactFile/ArtifactFileMarkdown.tsx
+++ frontend/app/layout/Artifact/ArtifactFile/ArtifactFileMarkdown.tsx
@@ -34,7 +34,7 @@ export default ({ file }: ArtifactFileMarkdownProps) => {
         <Typography className="text-sm">{file.name}</Typography>
       </Hstack>
 
-      <div className="p-3.5 text-[#BACAC4] whitespace-pre-line wrap-break-word">
+      <div className="p-3.5 text-[#BACAC4] whitespace-pre-line break-all">
         <Markdown content={data} />
       </div>
     </div>
