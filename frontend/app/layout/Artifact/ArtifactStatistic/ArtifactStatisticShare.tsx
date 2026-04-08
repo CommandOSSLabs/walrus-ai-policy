b686ca74fee33e2b4a1b08773501e37050a9946a
synasapmob <sonn51280@gmail.com>
2026-04-08 10:26:45 +0700

    responsive for ArtifactShare modal

--- frontend/app/layout/Artifact/ArtifactStatistic/ArtifactStatisticShare.tsx
+++ frontend/app/layout/Artifact/ArtifactStatistic/ArtifactStatisticShare.tsx
@@ -77,169 +77,174 @@ export default ({ artifact }: ArtifactStatisticShareProps) => {
 
       <DialogContent
         showCloseButton={false}
-        className="p-0 gap-0 bg-[#1A2130] border border-[#3B4A45] overflow-hidden w-130 flex flex-col"
+        onInteractOutside={(e) => e.preventDefault()}
+        className="flex justify-center ring-0"
         style={{
           maxWidth: "100%",
         }}
       >
-        <Center className="px-6 h-16 justify-between bg-[#141A28] border-b border-[#3B4A45]">
-          <Vstack>
-            <Typography font="grotesk" className="text-sm font-bold">
-              Share Artifact
-            </Typography>
+        <div className="bg-[#1A2130] border border-[#3B4A45] w-130 rounded-xl overflow-hidden">
+          <Center className="px-6 h-16 justify-between bg-[#141A28] border-b border-[#3B4A45]">
+            <Vstack>
+              <Typography font="grotesk" className="text-sm font-bold">
+                Share Artifact
+              </Typography>
 
-            <Typography font="jetbrains" className="text-[#84948F] text-xs">
-              Choose a platform to share on
-            </Typography>
-          </Vstack>
-
-          <DialogClose className="size-8 bg-[#1A2130] border border-[#3B4A45] rounded-sm">
-            <CloseLine className="size-3.5 mx-auto" />
-          </DialogClose>
-        </Center>
-
-        <Vstack className="px-6 py-4 gap-5">
-          <Center className="bg-[#0D111D] border border-[#352F2F] rounded-lg px-4 py-3 gap-6">
-            <Vstack className="gap-0.5">
-              <Flex className="gap-2">
-                <Typography
-                  font="grotesk"
-                  className="text-xs font-bold line-clamp-1"
-                >
-                  {artifact.title}
-                </Typography>
-
-                <Badge
-                  type="green"
-                  className="text-[0.5rem] h-3.5 px-1.5 whitespace-pre"
-                >
-                  {artifact.category}
-                </Badge>
-              </Flex>
-
-              <Typography
-                font="jetbrains"
-                className="text-2xs text-[#84948F] line-clamp-1"
-              >
-                {artifact.description}
+              <Typography font="jetbrains" className="text-[#84948F] text-xs">
+                Choose a platform to share on
               </Typography>
             </Vstack>
 
-            <Center className="bg-[#46F1CF]/10 border border-[#46F1CF]/20 rounded-sm px-3 h-full">
-              <Typography font="jetbrains" className="text-[#46F1CF] text-2xs">
-                v{artifact.version}
-              </Typography>
-            </Center>
+            <DialogClose className="size-8 bg-[#1A2130] border border-[#3B4A45] rounded-sm">
+              <CloseLine className="size-3.5 mx-auto" />
+            </DialogClose>
           </Center>
 
-          <Vstack className="gap-3">
-            <Typography
-              font="jetbrains"
-              className="text-[#84948F]/60 text-xs uppercase"
-            >
-              Select platform
-            </Typography>
+          <Vstack className="px-6 py-4 gap-5">
+            <Flex className="bg-[#0D111D] border border-[#352F2F] rounded-lg px-4 py-3 gap-6">
+              <Vstack className="gap-0.5">
+                <Flex className="gap-2">
+                  <Typography
+                    font="grotesk"
+                    className="text-xs font-bold line-clamp-1"
+                  >
+                    {artifact.title}
+                  </Typography>
 
-            <div className="grid grid-cols-2 gap-3">
-              {ListPlatform.map((meta) => (
-                <button
-                  key={meta.key}
-                  onClick={() => setSelect(meta.key)}
-                  className={tv({
-                    base: [
-                      meta.key === select
-                        ? "bg-white/4 border-white/25"
-                        : "bg-[#0D111D]/60 border-[#0088CC]/20",
-
-                      "p-3.5 border rounded-lg",
-                      "flex flex-col gap-2 items-start",
-                      "hover:bg-white/4 transition-colors",
-                    ],
-                  })()}
-                >
-                  {meta.icon}
-
-                  <Vstack className="gap-0.5 text-xs items-start">
-                    <Typography font="grotesk" className="font-bold">
-                      {meta.heading}
-                    </Typography>
-
-                    <Typography font="jetbrains" className="text-[#84948F]">
-                      {meta.description}
-                    </Typography>
-                  </Vstack>
-                </button>
-              ))}
-            </div>
-          </Vstack>
-
-          <Vstack>
-            <Typography
-              font="jetbrains"
-              className="text-xs text-[#84948F]/60 uppercase"
-            >
-              Or copy link
-            </Typography>
-
-            <Hstack className="min-w-0 text-[#84948F]">
-              <Hstack className="min-w-0 flex-1 bg-[#0D111D] rounded-lg px-3 h-9">
-                <LinkLine />
+                  <Badge
+                    type="green"
+                    className="text-[0.5rem] h-3.5 px-1.5 whitespace-pre"
+                  >
+                    {artifact.category}
+                  </Badge>
+                </Flex>
 
                 <Typography
                   font="jetbrains"
-                  className="min-w-0 flex-1 truncate text-2xs"
+                  className="text-2xs text-[#84948F] line-clamp-1"
                 >
-                  {shareURL}
+                  {artifact.description}
                 </Typography>
-              </Hstack>
+              </Vstack>
 
-              <button
-                onClick={() => handleCopy(shareURL)}
-                className={tv({
-                  base: [
-                    isCopy
-                      ? "bg-[#46F1CF]/10 border-[#46F1CF]/40 text-[#46F1CF]"
-                      : "bg-[#0D111D]/60 border-[#352F2F]",
+              <Center className="bg-[#46F1CF]/10 border border-[#46F1CF]/20 rounded-sm px-3 h-full">
+                <Typography
+                  font="jetbrains"
+                  className="text-[#46F1CF] text-2xs"
+                >
+                  v{artifact.version}
+                </Typography>
+              </Center>
+            </Flex>
 
-                    "flex items-center gap-2",
-                    "border rounded-lg h-9 px-3",
-                  ],
-                })()}
+            <Vstack className="gap-3">
+              <Typography
+                font="jetbrains"
+                className="text-[#84948F]/60 text-xs uppercase"
               >
-                {isCopy ? (
-                  <CheckedLine className="size-3.5" />
-                ) : (
-                  <CopyLine className="size-3.5" />
-                )}
+                Select platform
+              </Typography>
 
-                <Typography font="jetbrains" className="text-2xs">
-                  {isCopy ? "COPIED!" : "COPY"}
-                </Typography>
-              </button>
-            </Hstack>
+              <div className="grid grid-cols-2 gap-3">
+                {ListPlatform.map((meta) => (
+                  <button
+                    key={meta.key}
+                    onClick={() => setSelect(meta.key)}
+                    className={tv({
+                      base: [
+                        meta.key === select
+                          ? "bg-white/4 border-white/25"
+                          : "bg-[#0D111D]/60 border-[#0088CC]/20",
+
+                        "p-3.5 border rounded-lg",
+                        "hover:bg-white/4 transition-colors",
+                      ],
+                    })()}
+                  >
+                    {meta.icon}
+
+                    <Vstack className="gap-0.5 text-xs text-left mt-3">
+                      <Typography font="grotesk" className="font-bold">
+                        {meta.heading}
+                      </Typography>
+
+                      <Typography font="jetbrains" className="text-[#84948F]">
+                        {meta.description}
+                      </Typography>
+                    </Vstack>
+                  </button>
+                ))}
+              </div>
+            </Vstack>
+
+            <Vstack>
+              <Typography
+                font="jetbrains"
+                className="text-xs text-[#84948F]/60 uppercase"
+              >
+                Or copy link
+              </Typography>
+
+              <Hstack className="min-w-0 text-[#84948F]">
+                <Hstack className="min-w-0 flex-1 bg-[#0D111D] rounded-lg px-3 h-9">
+                  <LinkLine />
+
+                  <Typography
+                    font="jetbrains"
+                    className="min-w-0 flex-1 truncate text-2xs"
+                  >
+                    {shareURL}
+                  </Typography>
+                </Hstack>
+
+                <button
+                  onClick={() => handleCopy(shareURL)}
+                  className={tv({
+                    base: [
+                      isCopy
+                        ? "bg-[#46F1CF]/10 border-[#46F1CF]/40 text-[#46F1CF]"
+                        : "bg-[#0D111D]/60 border-[#352F2F]",
+
+                      "flex items-center gap-2",
+                      "border rounded-lg h-9 px-3",
+                    ],
+                  })()}
+                >
+                  {isCopy ? (
+                    <CheckedLine className="size-3.5" />
+                  ) : (
+                    <CopyLine className="size-3.5" />
+                  )}
+
+                  <Typography font="jetbrains" className="text-2xs">
+                    {isCopy ? "COPIED!" : "COPY"}
+                  </Typography>
+                </button>
+              </Hstack>
+            </Vstack>
           </Vstack>
-        </Vstack>
 
-        <Center className="px-6 py-3 justify-end bg-[#141A28] border-t border-[#3B4A45]">
-          <button
-            className="text-xs font-bold px-5 h-9 border border-white/25 rounded-full"
-            style={{
-              background:
-                "linear-gradient(135deg, rgba(255, 255, 255, 0.13) 0%, rgba(255, 255, 255, 0.07) 100%)",
-            }}
-            onClick={() => {
-              const getPlatform = ListPlatform.find(
-                (meta) => meta.key === select,
-              );
+          <Center className="px-6 py-3 justify-end bg-[#141A28] border-t border-[#3B4A45]">
+            <button
+              className="text-xs font-bold px-5 h-9 border border-white/25 rounded-full"
+              style={{
+                background:
+                  "linear-gradient(135deg, rgba(255, 255, 255, 0.13) 0%, rgba(255, 255, 255, 0.07) 100%)",
+              }}
+              onClick={() => {
+                const getPlatform = ListPlatform.find(
+                  (meta) => meta.key === select,
+                );
 
-              if (!getPlatform) return;
+                if (!getPlatform) return;
 
-              openSocialShare(getPlatform.key, shareURL);
-            }}
-          >
-            Share now
-          </button>
-        </Center>
+                openSocialShare(getPlatform.key, shareURL);
+              }}
+            >
+              Share now
+            </button>
+          </Center>
+        </div>
       </DialogContent>
     </Dialog>
   );
