c8fd021c914f5f4909257742eee3083ddb25cd64
synasapmob <sonn51280@gmail.com>
2026-04-14 11:14:58 +0700

    separate utils for reusable

--- frontend/app/hook/useWalBalance.ts
+++ frontend/app/hook/useWalBalance.ts
@@ -2,6 +2,7 @@ import { SUI_DECIMALS } from "@mysten/sui/utils";
 import { useQuery } from "@tanstack/react-query";
 import { BigNumber } from "bignumber.js";
 import utilsSui from "app/utils/utils.sui";
+import utilsWalrus from "app/utils/utils.walrus";
 
 export default (address: string | undefined) => {
   return useQuery({
@@ -10,9 +11,7 @@ export default (address: string | undefined) => {
     queryFn: async () => {
       const { balance } = await utilsSui.getSuiClient.getBalance({
         owner: address!,
-        coinType: utilsSui.isTestNet
-          ? "0x8270feb7375eee355e64fdb69c50abb6b5f9393a722883c1cf45f8e26048810a::wal::WAL"
-          : "0x356a26eb9e012a68958082340d4c4116e7f55615cf27affcff209cf0ae544f59::wal::WAL",
+        coinType: utilsWalrus.getCoinTypes,
       });
 
       return BigNumber(balance?.balance?.toString() ?? "0")
